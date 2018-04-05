ee3a34b4031cad67770075ec202c82fd85e8c7cc
yyccQQu <643929860@qq.com>
2018-04-05 15:37:22 +0800

    material md-toolbar

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { CoreModule } from "./core/core.module";
+import { MdSidenavModule } from "@angular/material";
 
 import { AppComponent } from "./app.component";
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, CoreModule],
+  imports: [BrowserModule, CoreModule, MdSidenavModule],
   providers: [],
   bootstrap: [AppComponent]
 })
