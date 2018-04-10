2b332c6492d0ee1ee66e22900862ac3f189f2614
yyccQQu <643929860@qq.com>
2018-04-10 22:54:50 +0800

    MdList 和 MdIcon的封装

--- my-app/src/app/core/sidebar/sidebar.component.ts
+++ my-app/src/app/core/sidebar/sidebar.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { getDate } from "date-fns";
 
 @Component({
-  selector: 'app-sidebar',
-  templateUrl: './sidebar.component.html',
-  styleUrls: ['./sidebar.component.scss']
+  selector: "app-sidebar",
+  templateUrl: "./sidebar.component.html",
+  styleUrls: ["./sidebar.component.scss"]
 })
 export class SidebarComponent implements OnInit {
+  today = "day";
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
+    this.today = `day${getDate(new Date())}`;
   }
-
 }
--- my-app/src/app/utils/svg.util.ts
+++ my-app/src/app/utils/svg.util.ts
@@ -1,6 +1,87 @@
 import { MdIconRegistry } from "@angular/material";
 import { DomSanitizer } from "@angular/platform-browser";
 
+/**
+ * a utility to load all needed svg resources to the app for md-icon to use
+ *
+ * @param ir a MdIconRegistry instance to use external svg resources for md-icon use
+ * @param ds a DomSanitizer instance to bypass security and return a url
+ */
 export const loadSvgResources = (ir: MdIconRegistry, ds: DomSanitizer) => {
-  ir.addSvgIcon("gifts", ds.bypassSecurityTrustResourceUrl("assets/add.svg"));
+  const imgDir = "assets/img";
+  const avatarDir = `${imgDir}/avatar`;
+  const sidebarDir = `${imgDir}/sidebar`;
+  const iconDir = `${imgDir}/icons`;
+  const dayDir = `${imgDir}/days`;
+  ir.addSvgIconSetInNamespace(
+    "avatars",
+    ds.bypassSecurityTrustResourceUrl(`${avatarDir}/avatars.svg`)
+  );
+  ir.addSvgIcon(
+    "unassigned",
+    ds.bypassSecurityTrustResourceUrl(`${avatarDir}/unassigned.svg`)
+  );
+  ir.addSvgIcon(
+    "project",
+    ds.bypassSecurityTrustResourceUrl(`${sidebarDir}/project.svg`)
+  );
+  ir.addSvgIcon(
+    "projects",
+    ds.bypassSecurityTrustResourceUrl(`${sidebarDir}/projects.svg`)
+  );
+  ir.addSvgIcon(
+    "month",
+    ds.bypassSecurityTrustResourceUrl(`${sidebarDir}/month.svg`)
+  );
+  ir.addSvgIcon(
+    "week",
+    ds.bypassSecurityTrustResourceUrl(`${sidebarDir}/week.svg`)
+  );
+  ir.addSvgIcon(
+    "day",
+    ds.bypassSecurityTrustResourceUrl(`${sidebarDir}/day.svg`)
+  );
+  ir.addSvgIcon(
+    "move",
+    ds.bypassSecurityTrustResourceUrl(`${iconDir}/move.svg`)
+  );
+  const days = [
+    1,
+    2,
+    3,
+    4,
+    5,
+    6,
+    7,
+    8,
+    9,
+    10,
+    11,
+    12,
+    13,
+    14,
+    15,
+    16,
+    17,
+    18,
+    19,
+    20,
+    21,
+    22,
+    23,
+    24,
+    25,
+    26,
+    27,
+    28,
+    29,
+    30,
+    31
+  ];
+  days.forEach(day =>
+    ir.addSvgIcon(
+      `day${day}`,
+      ds.bypassSecurityTrustResourceUrl(`${dayDir}/day${day}.svg`)
+    )
+  );
 };
