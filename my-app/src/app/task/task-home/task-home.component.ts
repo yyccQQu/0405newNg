6ba2720f67aedc32f3132d48d1f0cecaeeb00c5d
yyccQQu <643929860@qq.com>
2018-04-18 22:12:57 +0800

    任务列表之任务组件

--- my-app/src/app/task/task-home/task-home.component.ts
+++ my-app/src/app/task/task-home/task-home.component.ts
@@ -14,22 +14,28 @@ export class TaskHomeComponent implements OnInit {
         {
           id: 1,
           desc: "任务一: 去星巴克买咖啡",
+          completed: true,
+          priority: 3,
           owner: {
             id: 1,
             name: "张三",
             avatar: "avatars:svg-11"
           },
-          dueDate: new Date()
+          dueDate: new Date(),
+          reminder: new Date()
         },
         {
           id: 2,
           desc: "任务二:完成老师布置的ppt作业",
+          completed: false,
+          priority: 2,
           owner: {
             id: 1,
             name: "李四",
             avatar: "avatars:svg-12"
           },
-          dueDate: new Date()
+          dueDate: new Date(),
+          reminder: new Date()
         }
       ]
     },
@@ -40,16 +46,21 @@ export class TaskHomeComponent implements OnInit {
         {
           id: 1,
           desc: "任务三: 项目代码评审",
+          completed: false,
+          priority: 1,
           owner: {
             id: 1,
             name: "王五",
             avatar: "avatars:svg-13"
           },
-          dueDate: new Date()
+          dueDate: new Date(),
+          reminder: new Date()
         },
         {
           id: 2,
           desc: "任务四:制定项目计划",
+          completed: false,
+          priority: 2,
           owner: {
             id: 1,
             name: "李四",
--- my-app/src/app/task/task-item/task-item.component.ts
+++ my-app/src/app/task/task-item/task-item.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from "@angular/core";
 
 @Component({
-  selector: 'app-task-item',
-  templateUrl: './task-item.component.html',
-  styleUrls: ['./task-item.component.scss']
+  selector: "app-task-item",
+  templateUrl: "./task-item.component.html",
+  styleUrls: ["./task-item.component.scss"]
 })
 export class TaskItemComponent implements OnInit {
+  @Input() item;
+  @Input() avatar;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
+    this.avatar = this.item.owner ? this.item.owner.avatar : "unassigned";
   }
-
 }
